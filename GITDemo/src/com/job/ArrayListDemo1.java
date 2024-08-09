9f644de6f20cefe443c553362464b0409ab5a3f9
Himanshu Dhase <himanshudhase@Himanshus-Air.bwrouter>
2024-08-09 14:54:35 +0530

    add arraylistDemo1.java to GITDemo

--- GITDemo/src/com/job/ArrayListDemo1.java
+++ GITDemo/src/com/job/ArrayListDemo1.java
@@ -1,5 +1,17 @@
 package com.job;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo1 {
 
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("bangalore");
+		al.add("delhi");
+		
+		System.out.println(al);
+	}
 }
